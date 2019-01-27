a39780db3f52d9d09e12faa596bdea1a437c9d6a
pusceddudaniele <43929743+pusceddudaniele@users.noreply.github.com>
2019-01-27 17:30:37 +0100

    Implement SFML Input

--- src/Chip8_SFML.cpp
+++ src/Chip8_SFML.cpp
@@ -54,4 +54,161 @@ void Chip8_SFML::display(){
 
 
 void Chip8_SFML::handleKeyEvent(sf::Event e){
+    /*
+    Chip8 Key   Keyboard
+    ---------   ---------
+     1 2 3 C     1 2 3 4
+     4 5 6 D     q w e r
+     7 8 9 E     a s d f
+     A 0 B F     z x c v
+     */
+
+    switch(e.type){
+        //Key Pressed
+        case sf::Event::KeyPressed:
+            switch(e.key.code){
+                //ESC
+                case sf::Keyboard::Escape:
+                    running = false;
+                break;
+
+                case sf::Keyboard::Num1:
+                    pressKey(1);
+                break;
+
+                case sf::Keyboard::Num2:
+                    pressKey(2);
+                break;
+
+                case sf::Keyboard::Num3:
+                    pressKey(3);
+                break;
+
+                case sf::Keyboard::Num4:
+                    pressKey(0xC);
+                break;
+
+                case sf::Keyboard::Q:
+                    pressKey(4);
+                break;
+
+                case sf::Keyboard::W:
+                    pressKey(5);
+                break;
+
+                case sf::Keyboard::E:
+                    pressKey(6);
+                break;
+
+                case sf::Keyboard::R:
+                    pressKey(0xD);
+                break;
+
+                case sf::Keyboard::A:
+                    pressKey(7);
+                break;
+
+                case sf::Keyboard::S:
+                    pressKey(8);
+                break;
+
+                case sf::Keyboard::D:
+                    pressKey(9);
+                break;
+
+                case sf::Keyboard::F:
+                    pressKey(0xE);
+                break;
+
+                case sf::Keyboard::Z:
+                    pressKey(0xA);
+                break;
+
+                case sf::Keyboard::X:
+                    pressKey(0);
+                break;
+
+                case sf::Keyboard::C:
+                    pressKey(0xB);
+                break;
+
+                case sf::Keyboard::V:
+                    pressKey(0xF);
+                break;
+            }
+        break;
+
+        case sf::Event::KeyReleased:
+            switch(e.key.code){
+                //ESC
+                case sf::Keyboard::Escape:
+                    running = false;
+                break;
+
+                case sf::Keyboard::Num1:
+                    releaseKey(1);
+                break;
+
+                case sf::Keyboard::Num2:
+                    releaseKey(2);
+                break;
+
+                case sf::Keyboard::Num3:
+                    releaseKey(3);
+                break;
+
+                case sf::Keyboard::Num4:
+                    releaseKey(0xC);
+                break;
+
+                case sf::Keyboard::Q:
+                    releaseKey(4);
+                break;
+
+                case sf::Keyboard::W:
+                    releaseKey(5);
+                break;
+
+                case sf::Keyboard::E:
+                    releaseKey(6);
+                break;
+
+                case sf::Keyboard::R:
+                    releaseKey(0xD);
+                break;
+
+                case sf::Keyboard::A:
+                    releaseKey(7);
+                break;
+
+                case sf::Keyboard::S:
+                    releaseKey(8);
+                break;
+
+                case sf::Keyboard::D:
+                    releaseKey(9);
+                break;
+
+                case sf::Keyboard::F:
+                    releaseKey(0xE);
+                break;
+
+                case sf::Keyboard::Z:
+                    releaseKey(0xA);
+                break;
+
+                case sf::Keyboard::X:
+                    releaseKey(0);
+                break;
+
+                case sf::Keyboard::C:
+                    releaseKey(0xB);
+                break;
+
+                case sf::Keyboard::V:
+                    releaseKey(0xF);
+                break;
+            }
+        break; 
+    }
 }
